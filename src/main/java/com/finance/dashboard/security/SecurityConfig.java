8a2fb367c19b9ed35f25490cb2a87805a69abc22
Mohamed Imran RMN <141555789+mohamedimranrmn@users.noreply.github.com>
2026-04-05 04:00:34 +0530

    Refactor SecurityFilterChain to include JWT filter

--- src/main/java/com/finance/dashboard/security/SecurityConfig.java
+++ src/main/java/com/finance/dashboard/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfig {
@@ -15,19 +17,18 @@ public class SecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/users/**").hasRole("ADMIN")
-                .requestMatchers("/records").hasAnyRole("ADMIN", "ANALYST")
-                .requestMatchers("/records/**").hasAnyRole("ADMIN", "ANALYST")
-                .requestMatchers("/dashboard/**").hasAnyRole("ADMIN", "ANALYST", "VIEWER")
-                .anyRequest().authenticated()
-            );
-
-        return http.build();
+    http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/auth/**").permitAll()
+            .requestMatchers("/users/**").hasRole("ADMIN")
+            .requestMatchers("/records/**").hasAnyRole("ADMIN", "ANALYST")
+            .requestMatchers("/dashboard/**").hasAnyRole("ADMIN", "ANALYST", "VIEWER")
+            .anyRequest().authenticated()
+        )
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); 
+    return http.build();
     }
 }
